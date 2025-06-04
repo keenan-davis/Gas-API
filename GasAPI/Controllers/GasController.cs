5d0b823350f2830a98292c64bfeaa67891f78813
keenan-davis <st10201316@vcconnect.edu.za>
2025-06-04 16:04:14 +0200

    Commit files

---
+++ GasAPI/Controllers/GasController.cs
@@ -0,0 +1,37 @@
+﻿using GasAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GasController : ControllerBase
+    {
+        private static readonly List<Gas> Gases = new()
+        {
+            new Gas { Name = "LPG", MinLevel = 0.1, MaxLevel = 2.5 },
+            new Gas { Name = "Propane", MinLevel = 0.1, MaxLevel = 2.1 },
+            new Gas { Name = "Hydrogen", MinLevel = 0.01, MaxLevel = 0.1 },
+            new Gas { Name = "Methane", MinLevel = 0.1, MaxLevel = 5.0 },
+            new Gas { Name = "Smoke", MinLevel = 0.0, MaxLevel = 0.2 },
+            new Gas { Name = "CO", MinLevel = 0.0, MaxLevel = 35.0 },
+            new Gas { Name = "Alcohol", MinLevel = 0.01, MaxLevel = 0.1 },
+            new Gas { Name = "Ethanol", MinLevel = 0.01, MaxLevel = 0.3 },
+            new Gas { Name = "Benzine", MinLevel = 0.05, MaxLevel = 0.15 }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Gas>> GetAllGases()
+        {
+            return Ok(Gases);
+        }
+
+
+        [HttpGet("{name}")]
+        public ActionResult<Gas> GetGasByName(string name)
+        {
+            var gas = Gases.FirstOrDefault(g => g.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            return gas == null ? NotFound($"Gas {name}' not found."): Ok(gas);
+        }
+    }
+}
---
+++ GasAPI/Models/Gas.cs
@@ -0,0 +1,9 @@
+﻿namespace GasAPI.Models
+{
+    public class Gas
+    {
+        public string Name { get; set; }
+        public double MinLevel { get; set; }
+        public double MaxLevel { get; set; }
+    }
+}
